b70dac3a3fca10e4322496063bc5d74c0c4c5c3a
jensjvh <43621451+jensjvh@users.noreply.github.com>
2024-01-21 23:48:54 +0200

    Update README and create requirements.txt

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-####Submission for the Wolt Summer 2024 Engineering Internship, backend specific preliminary assignment.
+# Submission for the Wolt Summer 2024 Engineering Internship, backend specific preliminary assignment.
 
 The task of the preliminary backend assignment is to build an HTTP API to calculate a delivery fee. My submission is implemented using Python and the Flask framework.
 
+### Features
 
+
+### Prerequisites
+Flask 3.0.1
+
+### Installation
+Use the command `pip install -r requirements.txt`
+
+### How to run
--- api.py
+++ api.py
@@ -1,10 +1,28 @@
-from flask import Flask, jsonify, request
+from flask import Flask,json, request
+
+#Create a Flask app
 app = Flask(__name__)
 
+#POST request handler
 @app.route('/', methods=['POST'])
-def main():
-    values = request.get_json()
-    return str(values["cart_value"])
+def api():
+    """
+    Handle the main POST request to return a response containing
+    the delivery fee.
+
+    Expected JSON payload:
+    {
+        "cart_value": float,
+        "delivery_distance": float,
+        "number_of_items": int,
+        "time": str
+    }
+
+    Returns:
+    JSON response containing the calculated delivery fee.
+    """
+
+    pass
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
