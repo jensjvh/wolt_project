91b2c988c111abe2c5803c2fe4f15b388025c754
jensjvh <43621451+jensjvh@users.noreply.github.com>
2024-01-27 15:39:02 +0200

    Update request_testing.py

--- request_testing.py
+++ request_testing.py
@@ -1,8 +1,10 @@
 import requests
 import json
 
-#Run this file after running api.py in a separate terminal window
-#Prints the POST request response, the delivery fee calculated from the given data
+'''
+Run this file after running api.py in a separate terminal window
+Send a POST request payload, and prints the response
+'''
 
 url = "http://localhost:5000"
 data = {"cart_value": 790, "delivery_distance": 2235, "number_of_items": 4, "time": "2024-01-15T13:00:00Z"}
